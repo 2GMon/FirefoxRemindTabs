69ae5b189057f46f8e4822718aae59cfb226e64b
2GMon <2gmon.t@gmail.com>
2022-12-07 17:49:04 +0900

    react

---
+++ src/pageAction/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+const PageAction = () => {
+  return (
+    <div>ほげ</div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.body);
+
+root.render(<PageAction />);
--- webpack.config.mjs
+++ webpack.config.mjs
@@ -11,6 +11,9 @@ const __dirname = path.dirname(__filename);
 
 const src = path.resolve(__dirname, 'src');
 const dist = path.resolve(__dirname, 'build');
+const nodeModules = path.resolve(__dirname, 'node_modules');
+const reactUMD = path.resolve(nodeModules, 'react', 'umd');
+const reactDomUMD = path.resolve(nodeModules, 'react-dom', 'umd');
 
 const config = {
     mode: 'production',
@@ -24,8 +27,7 @@ const config = {
         "options.css": path.resolve(src, 'options/style.css'),
         browserAction: path.resolve(src, 'browserAction/script.ts'),
         "browserAction.css": path.resolve(src, 'browserAction/style.css'),
-        pageAction: path.resolve(src, 'pageAction/script.ts'),
-        "pageAction.css": path.resolve(src, 'pageAction/style.css'),
+        pageAction: path.resolve(src, 'pageAction/index.tsx'),
     },
     output: {
         path: dist,
@@ -38,6 +40,10 @@ const config = {
                 use: 'ts-loader',
             },
             {
+                test: /\.tsx$/,
+                use: 'ts-loader',
+            },
+            {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
@@ -46,11 +52,17 @@ const config = {
     resolve: {
         extensions: [ '.tsx', '.ts', '.js', ],
     },
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM'
+    },
     plugins: [
         new RemoveEmptyScriptsPlugin(),
         new CopyPlugin({
             patterns: [
-                { from: path.resolve(src, 'icons'), to: 'icons' }
+                { from: path.resolve(src, 'icons'), to: 'icons' },
+                { from: path.resolve(reactUMD, 'react.production.min.js'), to: 'react.production.min.js' },
+                { from: path.resolve(reactDomUMD, 'react-dom.production.min.js'), to: 'react-dom.production.min.js' },
             ]
         }),
         new MiniCssExtractPlugin({
@@ -64,7 +76,7 @@ const config = {
         new HtmlWebpackPlugin({
             template: path.resolve(src, 'pageAction/index.html'),
             filename: 'pageAction.html',
-            chunks: ['pageAction', 'pageAction.css']
+            chunks: ['pageAction']
         }),
         new HtmlWebpackPlugin({
             template: path.resolve(src, 'options/index.html'),
